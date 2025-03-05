d51ce89cd24f5451e6a225bf3bb4ad027ca7ff13
andrei <78017302+a4askada@users.noreply.github.com>
2025-03-05 21:28:29 +0300

    buld

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import './App.css';
-import styled from 'styled-components';
-
+import "./App.css";
+import styled from "styled-components";
 
 function App() {
-    return (
-        <div className="App">
-            <Title>Welcome to IT-INCUBATOR</Title>
-        </div>
-    );
+	return (
+		<div className="App">
+			<Title>Привет</Title>
+		</div>
+	);
 }
 
 export default App;
 
 const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: #e91e63;
+	font-size: 1.5em;
+	text-align: center;
+	color: #1b1014;
 `;
