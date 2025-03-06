e9957f2a5bf5775227f387781ec901884f516469
andrei <78017302+a4askada@users.noreply.github.com>
2025-03-06 23:52:41 +0300

    start header 06.03.25

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import styled from "styled-components";
+import {Header} from "./layout/header/Header";
 
 function App() {
 	return (
 		<div className="App">
-			<Title>Привет</Title>
+			<Header />
 		</div>
 	);
 }
@@ -14,5 +15,5 @@ export default App;
 const Title = styled.h1`
 	font-size: 1.5em;
 	text-align: center;
-	color: #1b1014;
+	color: #ca366c;
 `;
